241fd562314a9e587497a04772077d038bee3bfa
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-25 17:06:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array = array.unshift(element)
+  var array = array.unshift(element)
   return array
 }
 
@@ -16,6 +16,6 @@ function addElementToEndOfArray(array, element) {
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array = array.push(element)
+  var array = array.push(element)
   return array
 }
