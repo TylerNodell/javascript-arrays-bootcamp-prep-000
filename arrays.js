774f8be060d8747ff90809ac5bc4c17a1b6fee1d
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-25 17:13:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,7 +25,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift()
+  array = array.shift()
 }
 
 function removeElementFromBeginningOfArray(array) {
