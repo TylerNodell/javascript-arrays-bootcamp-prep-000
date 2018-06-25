66ba23c4466e1596b35b6e04ba8b035548fa101f
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-25 17:05:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  var array = array.unshift(element)
+  [element, ...array]
   return array
 }
 
